7c86b748ab32f5f5aea2b8364d729d99a07e5d92
junayedahamed <junayedahamed660@mail.com>
2023-12-23 02:16:22 +0600

    day1

---
+++ list.dart
@@ -0,0 +1,9 @@
+void main() {
+  var num = [1, 2, 3, 4, 5, 6];
+
+  print(num); //printing whole list
+
+  print(num[3]); //printing index wise
+
+  print(num.reversed);
+}
---
+++ map.dart
@@ -0,0 +1,10 @@
+void main() {
+  //map has two types of value first key  or k and last is value or v ,,,   map<k,v>mapname
+  Map<String, int> stuednt = {
+    "junayed": 25,
+    "Ajoy": 22,
+    "fav": 24
+  }; //fav is favourite value....
+  print(stuednt["fav"]); //printing value key wise
+  print(stuednt); //printing whole map
+}
