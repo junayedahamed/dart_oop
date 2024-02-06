a676393866f12905d3373423f0a62e9fd7bd111d
junayedahamed <junayedahamed660@mail.com>
2024-02-07 01:58:46 +0600

    final&const

---
+++ finalANDconstant.dart
@@ -0,0 +1,14 @@
+void main() {
+  final name = "junayed";
+  print(name);
+
+  // name = "Ahamed";//this gives an error
+
+  const pi = 3.14159;
+  late var r;
+  //pi = 3.1416;//here we can' t change the cons variable value
+
+  r = 5;
+  var area = pi * r * r;
+  print(area);
+}
