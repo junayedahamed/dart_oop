861756fdf7ef716f9979d4245227b71e6135e14d
junayedahamed <junayedahamed660@gmail.com>
2025-02-07 23:35:04 +0600

    Add duplicate API call to fetch user data in dio_get example

--- dcat/lib/dio/dio_get.dart
+++ dcat/lib/dio/dio_get.dart
@@ -4,4 +4,6 @@ void main() async {
   final dio = Dio();
   var response = await dio.get("https://jsonplaceholder.typicode.com/users/1");
   print(response.data);
+  var response2 = await dio.get("https://jsonplaceholder.typicode.com/users/1");
+  print(response2.data);
 }
