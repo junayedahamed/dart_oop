bcd8988ff0e71f46becd903502c4a04bcd8e11a1
junayedahamed <junayedahamed660@gmail.com>
2025-02-13 23:16:13 +0600

    Add a main function to evaluate and print the sign of a number

---
+++ dcat/lib/uni_assignment/con_statement.dart
@@ -0,0 +1,10 @@
+void main() {
+  int num = 0;
+  if (num < 0) {
+    print('Number is negative');
+  } else if (num > 0) {
+    print('Number is positive');
+  } else {
+    print('Number is zero');
+  }
+}
