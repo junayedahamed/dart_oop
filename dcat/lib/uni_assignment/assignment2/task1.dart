c979fb7c5a9e99257bfd6d2a9341875d6a5bb304
junayedahamed <junayedahamed660@gmail.com>
2025-02-24 23:19:56 +0600

    assignment progress

--- dcat/lib/uni_assignment/assignment2/task1.dart
+++ dcat/lib/uni_assignment/assignment2/task1.dart
@@ -1,1 +1,11 @@
-void main() {}
+//univarsity
+class University {
+  String dept;
+  int sn;
+  University(this.dept, this.sn);
+}
+
+void main() {
+  University u = University("CSE", 120);
+  print(u.dept);
+}
