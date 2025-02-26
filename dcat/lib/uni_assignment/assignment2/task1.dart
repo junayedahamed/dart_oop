2f84f79db9af76672dadfe789fb87438b11a771a
junayedahamed <junayedahamed660@gmail.com>
2025-02-26 23:59:44 +0600

    university assignment progress

--- dcat/lib/uni_assignment/assignment2/task1.dart
+++ dcat/lib/uni_assignment/assignment2/task1.dart
@@ -8,6 +8,8 @@ class University {
 void main() {
   University u = University("CSE", 120);
   University u2 = University("EEE", 190);
+  University u3 = University("EEE", 190);
+  print(u3.dept);
   print(u.dept);
   print(u2.dept);
 }
