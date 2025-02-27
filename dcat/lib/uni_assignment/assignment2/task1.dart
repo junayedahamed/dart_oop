95975c1639bcb2fabd95193c35d8ce84ec003c21
junayedahamed <junayedahamed660@gmail.com>
2025-02-27 23:12:39 +0600

    assignment progress

--- dcat/lib/uni_assignment/assignment2/task1.dart
+++ dcat/lib/uni_assignment/assignment2/task1.dart
@@ -1,5 +1,11 @@
+mixin TakenClass {
+  void takeClass() {
+    print("class on going....");
+  }
+}
+
 //univarsity
-class University {
+class University with TakenClass {
   String dept;
   int sn;
   University(this.dept, this.sn);
@@ -9,6 +15,7 @@ void main() {
   University u = University("CSE", 120);
   University u2 = University("EEE", 190);
   University u3 = University("EEE", 190);
+  u3.takeClass();
   print(u3.dept);
   print(u.dept);
   print(u2.dept);
