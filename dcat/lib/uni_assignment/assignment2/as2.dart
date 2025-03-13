3a0af96a199dd1ae1ff954f5f3f2b66f824c36b6
junayedahamed <junayedahamed660@gmail.com>
2025-03-13 22:07:41 +0600

    practiced operator function

--- dcat/lib/uni_assignment/assignment2/as2.dart
+++ dcat/lib/uni_assignment/assignment2/as2.dart
@@ -4,9 +4,14 @@ class Calulate {
   operator +(int num) {
     return val + num;
   }
+
+  operator -(int num) {
+    return val - num;
+  }
 }
 
 void main() {
   Calulate c = Calulate(10);
   print(c + 12);
+  print(c - 10);
 }
